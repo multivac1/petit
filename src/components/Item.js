edcba93c111921518cf32755fd178e2c44f0be9b
nfoster <nfoster@glamit.com.ar>
2020-10-29 00:32:30 -0300

    creo componentes para productos

---
+++ src/components/Item.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Item = () => {
+
+    const products = [
+        {
+        id: 1,
+        title: "Titulo prod 1",
+        price: 240
+        },
+        {
+        id: 2,
+        title: "Titulo prod 2",
+        price: 300
+        },
+         {
+        id: 3,
+        title: "Titulo prod 3",
+        price: 450
+        }
+    }];
+
+    return (
+
+
+
+    );
+
+}
+
+export default Item;
